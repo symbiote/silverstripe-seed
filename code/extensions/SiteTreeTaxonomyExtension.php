7fef8a5f1ddca87c179c66be505330296bd15756
Nathan Glasl <nathan@silverstripe.com.au>
2014-12-03 15:19:13 +1100

    Removing the base page requirement.

---
+++ code/extensions/SiteTreeTaxonomyExtension.php
@@ -0,0 +1,42 @@
+<?php
+
+class SiteTreeTaxonomyExtension extends DataExtension {
+
+	private static $many_many = array(
+		'Terms' => 'TaxonomyTerm'
+	);
+
+	public function updateCMSFields(FieldList $fields) {
+
+		$taxonomySourceFunction = function(){
+
+			$source = TaxonomyTerm::get()->exclude('ParentID', 0);
+			$result = array();
+			if($source->count()){
+				foreach ($source as $term) {
+					$result[$term->ID] = $term->getTaxonomyName() . ": $term->Title";
+				}
+			}
+			asort($result);
+			return $result;
+		};
+
+		$taxonomySource = $taxonomySourceFunction();
+
+		$fields->addFieldToTab(
+			'Root.Main',
+			ListBoxField::create('Terms', 'Terms', $taxonomySource, null, null, true)
+				->useAddNew(
+					'TaxonomyTerm',
+					$taxonomySourceFunction,
+					FieldList::create(
+						TextField::create('Name', 'Title'),
+						DropdownField::create('ParentID', 'Parent', TaxonomyTerm::get()->filter('ParentID', 0)->map()->toArray())
+							->setEmptyString('')
+					)
+				),
+			'Content'
+		);
+	}
+
+}
