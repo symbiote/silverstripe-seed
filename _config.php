be514c3cc00f17232c235154befd047ac0e179bb
Nathan Glasl <nathan@silverstripe.com.au>
2015-02-19 13:50:27 +1100

    Adding login recording and a password complexity validator.

--- _config.php
+++ _config.php
@@ -1,1 +1,12 @@
 <?php
+
+	// Add a password complexity validator.
+
+	$validator = new PasswordValidator();
+	$validator->minLength(10);
+	$validator->characterStrength(2 ,array(
+		'lowercase',
+		'uppercase',
+		'digits'
+	));
+	Member::set_password_validator($validator);
