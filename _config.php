21d0e36a68dc8634ad8a29190ea0a748a22f6d63
Nathan Glasl <nathan@silverstripe.com.au>
2015-06-11 12:01:39 +1000

    Removed the duplicate broken link reports.

--- _config.php
+++ _config.php
@@ -1,12 +1,24 @@
 <?php
 
-	// Add a password complexity validator.
+// Add a password complexity validator.
 
-	$validator = new PasswordValidator();
-	$validator->minLength(10);
-	$validator->characterStrength(2 ,array(
-		'lowercase',
-		'uppercase',
-		'digits'
-	));
-	Member::set_password_validator($validator);
+$validator = new PasswordValidator();
+
+// The minimum length may be lowered when required.
+
+$validator->minLength(10);
+$validator->characterStrength(2 ,array(
+	'lowercase',
+	'uppercase',
+	'digits'
+));
+Member::set_password_validator($validator);
+
+// Remove the duplicate broken link reports.
+
+SS_Report::add_excluded_reports(array(
+	'Multisites_SideReport_BrokenLinks',
+	'Multisites_SideReport_BrokenFiles',
+	'Multisites_SideReport_BrokenVirtualPages',
+	'Multisites_SideReport_BrokenRedirectorPages'
+));
