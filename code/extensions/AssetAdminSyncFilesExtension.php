2c1d93fd76da324e99a052ba3e6a27b3a6104b68
Nathan Glasl <nathan@silverstripe.com.au>
2015-07-29 12:45:08 +1000

    Removed the sync files button, to prevent possible issues around files disappearing from the database.

---
+++ code/extensions/AssetAdminSyncFilesExtension.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ *	Remove the blasphemy that is known as sync files!
+ */
+
+class AssetAdminSyncFilesExtension extends Extension {
+
+	public function updateEditForm($form) {
+
+		$form->Fields()->removeByName('SyncButton');
+	}
+
+}
