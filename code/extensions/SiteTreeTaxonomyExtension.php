a042652e91eb5ca16966611e353f85ef7d5daf08
Jake Bentvelzen <jake@silverstripe.com.au>
2016-05-04 17:51:59 +1000

    feat(TaxonomyTermExtension): Pulled out code necessary to create tag fields ba-sis style so that I could easily reuse it for personal FileTaxonomyExtension, Also added functionality that allows for easy-to-use and configure reverse tagging, ie. adding pages to the tag.

--- code/extensions/SiteTreeTaxonomyExtension.php
+++ code/extensions/SiteTreeTaxonomyExtension.php
@@ -7,36 +7,6 @@ class SiteTreeTaxonomyExtension extends DataExtension {
 	);
 
 	public function updateCMSFields(FieldList $fields) {
-
-		$taxonomySourceFunction = function(){
-
-			$source = TaxonomyTerm::get()->exclude('ParentID', 0);
-			$result = array();
-			if($source->count()){
-				foreach ($source as $term) {
-					$result[$term->ID] = $term->getTaxonomyName() . ": $term->Title";
-				}
-			}
-			asort($result);
-			return $result;
-		};
-
-		$taxonomySource = $taxonomySourceFunction();
-
-		$fields->addFieldToTab(
-			'Root.Main',
-			ListBoxField::create('Terms', 'Terms', $taxonomySource, null, null, true)
-				->useAddNew(
-					'TaxonomyTerm',
-					$taxonomySourceFunction,
-					FieldList::create(
-						TextField::create('Name', 'Title'),
-						DropdownField::create('ParentID', 'Parent', TaxonomyTerm::get()->filter('ParentID', 0)->map()->toArray())
-							->setEmptyString('')
-					)
-				),
-			'Content'
-		);
+		$fields->addFieldToTab('Root.Main', TaxonomyTermExtension::create_field('Terms', 'Terms'), 'Content');
 	}
-
 }
