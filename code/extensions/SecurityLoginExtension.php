b065ebd82da53009d273aa7e989191f701485244
Nathan Glasl <nathan@silverstripe.com.au>
2017-02-16 15:46:17 +1100

    SECURITY, fixing a possible phishing exploit.

--- code/extensions/SecurityLoginExtension.php
+++ code/extensions/SecurityLoginExtension.php
@@ -7,7 +7,7 @@ class SecurityLoginExtension extends Extension {
 		// Determine whether a user has already authenticated, and whether a back URL exists.
 
 		$URL = $this->owner->getRequest()->getVar('BackURL');
-		if(Member::currentUserID() && $URL) {
+		if(Member::currentUserID() && $URL && Director::is_site_url($URL)) {
 
 			// Take the user to the back URL.
 
